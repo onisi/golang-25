e5ca84e6304fde523a19732b31cad7eb0142fbc6
onisi <onisi@omu.ac.jp>
2025-11-20 09:13:49 +0900

    Implement HTTP handlers for various endpoints
    Added multiple HTTP handlers for different routes including '/hello', '/now', '/dice', '/headers', and '/headera'.

--- week03/main.go
+++ week03/main.go
@@ -1,13 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 func main() {
-	// Week 03: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 03 課題")
-	
-	// 以下に実装してください
-	
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/now", nowhandler)
+	http.HandleFunc("/dice", dicehandler)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/headera", headera) //追加
+
+	http.ListenAndServe(":8980", nil)
+}
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "こんにちは from Cocespace !")
+}
+func nowhandler(w http.ResponseWriter, r *http.Request) {
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	fmt.Fprintln(w, (time.Now().In(jst)).Format("2006年01月02日 15:04:05"))
+}
+func dicehandler(w http.ResponseWriter, r *http.Request) {
+	seed := time.Now().UnixNano()
+	d := rand.New(rand.NewSource(seed))
+	fmt.Fprintln(w, d.Int31n(5)+1)
+}
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
+}
+// 一部ヘッダを取得する例．追加
+func headera(w http.ResponseWriter, r *http.Request) {
+	var h map[string][]string
+	h = r.Header
+	fmt.Fprintln(w, h["Accept-Encoding"][0])
 }
