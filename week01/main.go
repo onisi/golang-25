5f7a012ec1542e3da625098f63df25bb02271908
onisi <onisi@omu.ac.jp>
2025-11-20 09:25:04 +0900

    Set up HTTP server and display Go version
    Added HTTP server setup and Go version display.

--- week01/main.go
+++ week01/main.go
@@ -1,13 +1,16 @@
 package main
-
-import "fmt"
-
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+)
 func main() {
-	// Week 01: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 01 課題")
-	
-	// 以下に実装してください
-	
-}
+	fmt.Printf("Go version: %s\n", runtime.Version())
+
+	http.Handle("/", http.FileServer(http.Dir("public/")))
+
+	fmt.Println("Launch server...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Failed to launch server: %v", err)
+	}
+}   
